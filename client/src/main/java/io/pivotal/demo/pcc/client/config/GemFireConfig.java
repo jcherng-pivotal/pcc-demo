c2c92869accfdb8a46bb3a53945b9416773631fe
Jeff Cherng <jcherng@pivotal.io>
2018-12-11 19:27:17 -0600

    configure client by using ClientCacheApplication

--- client/src/main/java/io/pivotal/demo/pcc/client/config/GemFireConfig.java
+++ client/src/main/java/io/pivotal/demo/pcc/client/config/GemFireConfig.java
@@ -4,13 +4,13 @@ import io.pivotal.demo.pcc.repository.gf.CustomerOrderRepository;
 import org.apache.geode.cache.GemFireCache;
 import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
 import org.springframework.data.gemfire.config.annotation.EnableEntityDefinedRegions;
 import org.springframework.data.gemfire.config.annotation.EnablePdx;
 import org.springframework.data.gemfire.function.execution.GemfireOnRegionFunctionTemplate;
 import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 
-@Configuration
+@ClientCacheApplication
 @EnablePdx
 @EnableEntityDefinedRegions(basePackages = {"io.pivotal.demo.pcc.model.gf.pdx"},
         clientRegionShortcut = ClientRegionShortcut.PROXY)
---
+++ client/src/test/java/io/pivotal/demo/pcc/client/config/GemFireConfigTest.java
@@ -0,0 +1,23 @@
+package io.pivotal.demo.pcc.client.config;
+
+import org.apache.geode.cache.GemFireCache;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {GemFireConfig.class})
+public class GemFireConfigTest {
+    @Autowired
+    private GemFireCache cache;
+
+    @Test
+    public void testGemFireCache() {
+        assertThat(cache, notNullValue());
+    }
+}
